2828cd33c2e92f7d88c782c96b413180413c6c51
Gogs <gogs@fake.local>
2017-09-21 13:54:20 +0200

    with influx + grafana test

--- CM dynamics.py
+++ CM dynamics.py
@@ -132,10 +132,47 @@ def calcB(procNum):
     return [p, errors]
 
 
-def calcC(procNum):
+def calc_gamma(procNum):
+
+    lock.acquire()
+    np.random.seed( int( time.time() ) + procNum )
+    lock.release()
+
+    errors = 0
+    A = np.zeros(int(np.ceil(tmax/dt)+1))
+    B = np.zeros(int(len(A)))
+
+    taus = np.random.exponential(size=N)
+    ms = gengamma.rvs(1/c, c, size=N)
+
+    for m, tau in zip(ms, taus):   # Subsidiary trajectories
+        for i in xrange(stat):
+            u0 = 0; t = 0
+            while(t < tmax):
+                try:
+                    u0 += - u0 * dt / (tau*m) + np.sqrt(2.*dt) * \
+                         np.random.normal()
+                except FloatingPointError:
+                    errors += 1
+                    u0 = 0
+                    break
+                t += dt;
+                try:
+                    A[int(t/dt)] += (u0/(tau*m)) ** 2
+                    B[int(t/dt)] += (u0*m) ** 2
+                except FloatingPointError:
+#                    print('error')
+                    errors += 1
+                    break
+                    A[int(t/dt)] = 0
+                    B[int(t/dt)] = 0
+
+    return [A, B, errors]
+
+
+def calcC(procNum): # rewrite
 
     p = dict()
-    V = dict()
     for n in ns:
         p[n] = pdf_hist(1e-4, 1e12, 64, True)
 
@@ -146,8 +183,6 @@ def calcC(procNum):
     errors = 0
 
     for i in xrange(stat):
-        for n in ns:
-            V[n] = 0
 
         if procNum==0:
             if (i * 100) % stat == 0:
@@ -159,26 +194,19 @@ def calcC(procNum):
         taus = np.random.exponential(size=N)
         ms = gengamma.rvs(1/c, c, size=N)
 
-        for m, tau in zip(ms, taus):   # Subsidiary trajectories
-            u = 0; t = 0
-            while(t < tmax):
-                try:
-                    u += - u * dt / (tau*m) + np.sqrt(2.*dt) * \
-                         np.random.normal()
-                except FloatingPointError:
-                    errors += 1
-                    u = TRIM_RESULT
-                t += dt;
-                n = round(t/dt)
-                if n in ns:
-                    try:
-                        V[n] += m * u
-                    except FloatingPointError:
-                        V[n] = TRIM_RESULT
+        V = 0; t = 0
+        while(t < tmax):
+            t += dt;
+            n = round(t/dt)
+            try:
+                V += - V * dt * gammaeff[n] + np.sqrt(2.*dt) * \
+                     np.random.normal()
+            except FloatingPointError:
+                errors += 1
+                V = TRIM_RESULT
 
-        for n in ns:
-            V[n] *= np.sqrt(1./N*sum(map(lambda m: 1/m**2, ms))) / sum(ms)
-            p[n].inc_y(V[n])
+            if n in ns:
+                p[n].inc_y(V)
 
     return [p, errors]
 
@@ -188,14 +216,14 @@ def calcC(procNum):
 
 np.seterr(over='raise', invalid='raise')
 
-N = int(1e3)
-stat = int(8e1)
+N = int(1e0)
+stat = int(4e1)
 c = 0.75
 
-dt = 1e-3; tmax = 1e-1
-ns = map(round, np.logspace(-2, np.log10(tmax), 5)/dt)
+dt = 1e-6; tmax = 1e-1
+ns = map(round, np.logspace(-3, np.log10(tmax), 5)/dt)
 
-processors = 4   #multiprocessing.cpu_count()
+processors = 1   #multiprocessing.cpu_count()
 stat /= processors
 print('Using {:} processors'.format(processors))
 
@@ -213,7 +241,7 @@ signal.signal(signal.SIGINT, original_sigint_handler)
 t1 = time.time()
 
 try:
-    res = pool.map_async(calcC, range(processors))
+    res = pool.map_async(calc_gamma, range(processors))
     print("Waiting for results")
     sys.stdout.flush()
     result = res.get(2678400)
@@ -232,28 +260,56 @@ else:
     pool.close()
 pool.join()
 
+if True:
+    # C:
 
-# In[ ]:
+    ###
+
+    A = result[0][0]
+    B = result[0][1]
+    errs = result[0][2]
+
+    for res in result[1:]:
+ #       A += res[0]
+#        B += res[1]
+        errs += res[2]
+
+    print('{:} errors'.format(errs))
+
+    A[B==0] = 0
+
+    A[B!=0] /= B[B!=0]
+    A = np.sqrt(A)
+
+    del B
+
+    # put here usage of gamma(t)
 
 
-pdfs = result[0][0]
-errs = result[0][1]
 
-for res in result[1:]:
-    pdfstmp = res[0]
-    errs += res[1]
-    for key in pdfstmp:
-        pdfs[key].join(pdfstmp[key])
+    with open('taueff.pkl', 'wb') as ff:
+        pickle.dump(A, ff)
 
-for key in pdfs:
-    pdfs[key].normalize()
+else:
+    # A, B:
+    pdfs = result[0][0]
+    errs = result[0][1]
+
+    for res in result[1:]:
+        pdfstmp = res[0]
+        errs += res[1]
+        for key in pdfstmp:
+            pdfs[key].join(pdfstmp[key])
+
+    for key in pdfs:
+        pdfs[key].normalize()
+
+    with open('pdfsC.pkl', 'wb') as ff:
+        pickle.dump(pdfs, ff)
 
 
-print('{:} errors'.format(errs))
 
 
-with open('out/pdfsC.pkl', 'wb') as ff:
-    pickle.dump(pdfs, ff)
 
 
 # In[ ]:
